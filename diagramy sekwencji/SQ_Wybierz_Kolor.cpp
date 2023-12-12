607e069a61bac41c759e3e8990ae5a9dd9233994
WojciechJ <wjurgiel@student.agh.edu.pl>
2023-12-12 09:51:42 +0100

    Diagramy sekwencji wojtek

--- diagramy sekwencji/SQ_Wybierz_Kolor.cpp
+++ diagramy sekwencji/SQ_Wybierz_Kolor.cpp
@@ -1,3 +1,4 @@
+#define PLAYERS 4
 #include <iostream>
 #include <vector>
 using std::cout;
@@ -8,7 +9,8 @@ using std::vector;
 
 class Przeciwnik{};
 class Komputer{};
-
+class Lobby{};
+class Kolor{};
 class System{
     public:
     vector<string> nazwa_gracza;
@@ -23,6 +25,8 @@ class System{
     //funkcje dodane na biezaco na potrzeby diagramu sekwencji
     void zacznijGre();
     string stworzLobby(); //return lobby_id
+    Lobby porzucLobby();  // return 
+    
 };
 
 class Gra{
@@ -40,26 +44,99 @@ class Gra{
     void przeanalizujInformacje();
     
 };
+
 class Kolor{
     public:
-    string wybierzKolor();
-    bool czyKolorDostepny();
+    bool Get_isLobby;
+    vector<string> inUse;
+    string color;
+
+    string Set_statusInfo(bool isLobby);
+  
+    bool Set_isColorAvailable();
+    string Set_pickColor(Lobby l,string name); // only when isColorAvailable == true
+
 };
 class Lobby{
     public:
-    bool czyLobbyIstnieje(string);
-    string zwrocInfo();
-    Kolor ustawKolor();
-
-};
-bool Lobby::czyLobbyIstnieje(string lobby_id){
-    
+    string Get_statusInfo; 
+    bool Get_isColorAvailable;
+    string Get_pickedColor;
+    int timer;
+    string names[4];
+    string get_playerCurrentlyPicking;
+    //bool Set_isLobby(Lobby*);
+    string Set_ColorToPlayer();
+    void timeTick();
+    string Lobby::set_playerCurrentlyPicking(int id);
+    Lobby(System s){
+        names[0] = s.nazwa_gracza[0];
+        names[1] = s.nazwa_gracza[1];
+        names[2] = s.nazwa_gracza[2];
+        names[3] = s.nazwa_gracza[3];
+    }
+};  
+string Lobby::set_playerCurrentlyPicking(int id){
+    return names[id];
+}
+bool Set_isLobby(Lobby* lobby){
+    return (lobby !=  nullptr);
+}
+string Kolor::Set_statusInfo(bool isLobby){
+    std::string retMessage;
+    (isLobby) ? retMessage = "Lobby exists\n" : retMessage = "[ERROR]: Lobby doesn't exist or there are major problems with it. Further connections aborted.\n"; 
+    return retMessage;
+}
+bool Kolor::Set_isColorAvailable(){
+    for(int i = 0; i < inUse.size(); i++){
+        if(color == inUse[i])
+            return false;
+    }
+    return true;
+}
+string Kolor::Set_pickColor(Lobby l, string name){
+    l.get_playerCurrentlyPicking = name;
+    return color;
+}
+string Lobby::Set_ColorToPlayer(){
+    return Get_pickedColor;
 }
 
-
-
 int main(){
+    
+    System server;
+    Gra game;
+    Kolor colorHandler;
+    Lobby lobby(server);
+    Przeciwnik opponent;
+    Komputer cpu;
 
+    colorHandler.Get_isLobby = Set_isLobby(&lobby); //1.CzyLobbyIstnieje()
+    lobby.Get_statusInfo = colorHandler.Set_statusInfo(colorHandler.Get_isLobby); //1.1 ZwrocInfo()
+    if(colorHandler.Get_isLobby){
+        lobby.get_playerCurrentlyPicking = lobby.set_playerCurrentlyPicking(2); // gracz nr 3
+        string name = lobby.get_playerCurrentlyPicking;
+        colorHandler.color = colorHandler.Set_pickColor(lobby, name); //1.2 pickColor()
+        lobby.Get_pickedColor = colorHandler.color;
+        lobby.Get_isColorAvailable = colorHandler.Set_isColorAvailable(); //1.3isColorAvailable()
+
+        if(lobby.Get_isColorAvailable && time > 0){
+            for(int players = 0; players<PLAYERS; players++){
+                server.nazwa_gracza.push_back(name);
+                server.kolor_gracza.push_back(lobby.Set_ColorToPlayer()); //1.3.1 Set_ColorToPlayer()
+            }
+
+        }else{
+            bool full = server.kolor_gracza.size() == 4;
+            if(full){
+                server.zacznijGre();
+            }
+        }
+        
+    }
+    else{
+        server.porzucLobby();
+    }
 
     return 0;
 }
